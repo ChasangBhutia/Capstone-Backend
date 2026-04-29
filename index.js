e4b1b5d0bb38e70f5d993ddae3ce9f195af51453
ChasangBhutia <chasangtsering@gmail.com>
2026-04-29 17:00:59 +0530

    Fix: cors

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const server = http.createServer(app);
 // Initialize Socket.IO
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'https://capstone-frontend-tan-sigma.vercel.app',
         credentials: true
     }
 });
@@ -37,7 +37,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://capstone-frontend-tan-sigma.vercel.app',
     credentials: true
 }));
 
