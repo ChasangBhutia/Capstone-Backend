c70a365cdc64da619bff9356176ee5f7aa93fb2e
ChasangBhutia <chasangtsering@gmail.com>
2026-04-29 16:56:24 +0530

    Fix: open router backend url

--- config/openRouterConfig.js
+++ config/openRouterConfig.js
@@ -4,7 +4,7 @@ const openRouterConfig = axios.create({
     baseURL: "https://openrouter.ai/api/v1",
     headers: {
         "Authorization": `Bearer ${process.env.OPEN_ROUTER_KEY}`,
-        "HTTP-Referer": "http://localhost:3000",
+        "HTTP-Referer": "https://capstone-backend-2yau.onrender.com",
         "Content-Type": "application/json"
     }
 })
